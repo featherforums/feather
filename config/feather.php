11825a16f35ae0713989618c4e86bec36f2bc53f
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-22 01:05:17 +0930

    Combined the configuration files into a single file.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ config/feather.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Feather Configuration
+|--------------------------------------------------------------------------
+|
+| This is the main configuration file for Feather. Generally the only
+| items you will need to configure is the database connection.
+|
+*/
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Database Connection
+	|--------------------------------------------------------------------------
+	|
+	| Your database connection information.
+	|
+	*/
+
+	'database' => array(
+		'host'	   => 'localhost',
+		'database' => 'feather',
+		'username' => 'root',
+		'password' => '',
+		'prefix'   => '',
+		'charset'  => 'utf8',
+		'driver'   => 'mysql'
+	),
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Feather Components
+	|--------------------------------------------------------------------------
+	|
+	| Components to be registered at runtime with Feather. It is advised you
+	| do not edit anything down there.
+	|
+	*/
+
+	'components' => array()
+
+);
