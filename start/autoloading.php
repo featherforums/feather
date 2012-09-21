6260546d9a37d2b29e72616aa758fec52a81a21e
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-22 00:33:25 +0930

    Namespaced the autoloading start script.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- start/autoloading.php
+++ start/autoloading.php
@@ -1,4 +1,15 @@
-<?php
+<?php namespace Feather;
+
+use Autoloader;
+
+/*
+|--------------------------------------------------------------------------
+| Autoloader Namespaces
+|--------------------------------------------------------------------------
+|
+| Register the Feather namespaces with the Laravel autoloader.
+|
+*/
 
 Autoloader::namespaces(array(
 	'Feather' => path('feather')
