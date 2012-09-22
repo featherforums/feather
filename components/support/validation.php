48bf9ce3446e86e96b4e9740f4a1af1d8f5dfed0
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-23 02:01:59 +0930

    Initial commit of validation component.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ components/support/validation.php
@@ -0,0 +1,185 @@
+<?php namespace Feather\Components\Support;
+
+use DB;
+use Validator;
+use Laravel\Messages;
+use InvalidArgumentException;
+use FeatherValidationException;
+use Feather\Components\Foundation\Component;
+
+class Validation extends Component {
+
+	/**
+	 * Application to load messages from.
+	 * 
+	 * @var string
+	 */
+	public $application = 'core';
+
+	/**
+	 * The items currently validating.
+	 * 
+	 * @var array
+	 */
+	public $validating;
+
+	/**
+	 * Input to validate against.
+	 * 
+	 * @var array
+	 */
+	public $input = array();
+
+	/**
+	 * Rules to use for validation.
+	 * 
+	 * @var array
+	 */
+	public $rules = array();
+
+	/**
+	 * Error messages to use for validation.
+	 * 
+	 * @var array
+	 */
+	public $messages = array();
+
+	/**
+	 * Data to be used when setting rules.
+	 * 
+	 * @var array
+	 */
+	public $data = array();
+
+	/**
+	 * Sets the application.
+	 * 
+	 * @param  string  $application
+	 * @return Feather\Components\Support\Validation
+	 */
+	public function app($application)
+	{
+		$this->application = $application;
+
+		return $this;
+	}
+
+	/**
+	 * Get a validator.
+	 * 
+	 * @param  string  $validator
+	 * @return Feather\Components\Support\Validation
+	 */
+	public function get($validator)
+	{
+		if(!$this->validating[$validator] = $this->feather['config']->get("feather: validation.{$validator}"))
+		{
+			throw new InvalidArgumentException("Invalid validator [{$validator}] supplied.");
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Sets input to validate against.
+	 * 
+	 * @param  array  $input
+	 * @return Feather\Components\Support\Validation
+	 */
+	public function against($input)
+	{
+		$this->input = $input;
+
+		return $this;
+	}
+
+	/**
+	 * Executes validation and returns true on success.
+	 * 
+	 * @return bool
+	 */
+	public function passes()
+	{		
+		// If a response is returned from the closure then we have a custom error messages
+		// to throw with the exception. 
+		if($responses = call_user_func(reset($this->validating), $this))
+		{
+			if(!is_array(reset($responses))) $responses = array(array_shift($responses) => array());
+
+			foreach($responses as $response => $replacements)
+			{
+				$responses[$response] = __("{$this->application}::{$response}", $replacements)->get();
+			}
+
+			throw new FeatherValidationException(new Messages($responses));
+		}
+
+		$event = key($this->validating);
+
+		$this->feather['gear']->fire("validation: before {$event}", array($this));
+
+		$validator = new Validator($this->input, $this->rules, $this->messages);
+
+		if($validator->connection(DB::connection(FEATHER_DATABASE))->fails())
+		{
+			throw new FeatherValidationException($validator->errors);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Adds a rule or array of rules to the rules array.
+	 * 
+	 * @param  string        $name
+	 * @param  array|string  $rule
+	 * @return Feather\Components\Support\Validation
+	 */
+	public function rule($name, $rule)
+	{
+		if(!isset($this->rules[$name]))
+		{
+			$this->rules[$name] = array();
+		}
+
+		$this->rules[$name] = array_merge($this->rules[$name], (array) $rule);
+
+		return $this;
+	}
+
+	/**
+	 * Adds a message to the messages array.
+	 * 
+	 * @param  string        $rule
+	 * @param  array|string  $message
+	 * @return Feather\Components\Support\Validation
+	 */
+	public function message($rule, $message)
+	{
+		$replacements = array();
+
+		if(is_array($message))
+		{
+			list($message, $replacements) = $message;
+		}
+
+		$this->messages[str_replace('.', '_', $rule)] = __("{$this->application}::{$message}", $replacements)->get();
+
+		return $this;
+	}
+
+	/**
+	 * Adds a data key/value pair to the data array.
+	 * 
+	 * @param  string  $key
+	 * @param  mixed   $value
+	 * @return Feather\Components\Support\Validation
+	 */
+	public function with($key, $value)
+	{
+		$this->data[$key] = $value;
+
+		return $this;
+	}
+
+}
