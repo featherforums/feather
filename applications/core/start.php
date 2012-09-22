d4cfbcd8009bc9d1a0dac5295739378a3cac177c
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-22 14:36:43 +0930

    Load Feather instance and register autoloaders in core start.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- applications/core/start.php
+++ applications/core/start.php
@@ -1,1 +1,27 @@
-<?php
+<?php namespace Feather;
+
+use Autoloader;
+
+/*
+|--------------------------------------------------------------------------
+| Feather Facade Instance
+|--------------------------------------------------------------------------
+|
+| Get the Feather instance.
+|
+*/
+
+$feather = Components\Support\Facade::application();
+
+/*
+|--------------------------------------------------------------------------
+| Core Autoloadings
+|--------------------------------------------------------------------------
+|
+| Register the core namespaces and mappings with Laravel's autoloader.
+|
+*/
+
+Autoloader::namespaces(array(
+	'Feather\\Models' => path('core') . 'models'
+));
