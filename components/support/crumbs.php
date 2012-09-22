af3284e6e98dc12a86606555168058969e75587d
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-23 02:01:24 +0930

    Initial commit of crumbs component.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ components/support/crumbs.php
@@ -0,0 +1,103 @@
+<?php namespace Feather\Components\Support;
+
+use URL;
+use HTML;
+use Feather\Models;
+use Feather\Components\Foundation\Component;
+
+class Crumbs extends Component {
+
+	/**
+	 * Crumbs to be dropped on the trail.
+	 * 
+	 * @var array
+	 */
+	public $crumbs = array();
+
+	/**
+	 * Drop a crumb.
+	 * 
+	 * @param  array|object  $crumb
+	 * @return void
+	 */
+	public static function drop($crumb)
+	{
+		if($crumb instanceof Models\Place)
+		{
+			$this->place($crumb);
+		}
+		else
+		{
+			// If the crumb is an object, such as a Laravel Messages object, then convert it
+			// to a string as any objects being passed to this method will have a __toString()
+			if(is_object($crumb))
+			{
+				$crumb = array((string) $crumb);
+			}
+
+			// Fetch the title of the crumb and the crumbs link. If the crumb does not have
+			// a supplied link then use the current page.
+			$title = isset($crumb['title']) ? $crumb['title'] : array_shift($crumb);
+
+			$link = isset($crumb['link']) ? $crumb['link'] : URL::current();
+
+			$this->crumbs[] = (object) compact('link', 'title');
+		}
+	}
+
+	/**
+	 * Leaves a trail of crumbs.
+	 * 
+	 * @return string
+	 */
+	public function trail($element = 'li')
+	{
+		$response = array(
+			$this->item(URL::to_route('feather'), $this->feather['config']->get('feather: db.forum.title'), $element)
+		);
+
+		foreach($this->crumbs as $crumb)
+		{
+			$response[] = $this->item($crumb->link, $crumb->title, $element);
+		}
+
+		return implode(PHP_EOL, $response);
+	}
+
+	/**
+	 * Adds a place to the trail.
+	 * 
+	 * @param  Feather\Models\Place  $place
+	 * @return array
+	 */
+	protected function place($place)
+	{
+		// Add each of the ancestors of this place to the crumbs.
+		foreach($place->crumbs() as $crumb)
+		{
+			$this->crumbs[] = array(
+				'link'  => URL::to_route('place', array($crumb->id, $crumb->slug)),
+				'title' => $crumb->name
+			);
+		}
+
+		return $this->crumbs[] = array(
+			'link'  => URL::to_route('place', array($place->id, $place->slug)),
+			'title' => $place->name
+		);
+	}
+
+	/**
+	 * Return a list item with the link.
+	 * 
+	 * @param  string  $link
+	 * @param  string  $text
+	 * @param  string  $element
+	 * @return string
+	 */
+	public function item($link, $text, $element = 'li')
+	{
+		return "<{$element}>" . HTML::link($link, $text) . "</{$element}>";
+	}
+
+}
