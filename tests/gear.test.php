19aaa2a4952e6d5febef4d0053011c2bd60f89a0
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-23 00:14:44 +0930

    Refactoring gears tests.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- tests/gear.test.php
+++ tests/gear.test.php
@@ -2,52 +2,43 @@
 
 class GearTest extends PHPUnit_Framework_TestCase {
 
+	public $feather;
+
+	public function setUp()
+	{
+		$this->feather = Feather\Components\Support\Facade::application();
+
+		$this->feather['gear']->register(new Feather\Models\Gear(array(
+			'identifier' => 'stub',
+			'location'	 => 'path: ' . __DIR__ . DS . 'mock',
+			'auto'		 => false
+		)));
+	}
+
 	public function testCanRegisterGear()
 	{
-		$feather = $this->stub();
-
-		$this->assertTrue($feather['gear']->registered('stub'));
+		$this->assertTrue($this->feather['gear']->registered('stub'));
 	}
 
 	public function testCanStartGear()
 	{
-		$feather = $this->stub();
-
-		$this->assertInstanceOf('Feather\\Components\\Gear\\Container', $gear = $feather['gear']->start('stub'));
-		$this->assertTrue($feather['gear']->started('stub'));
+		$this->assertInstanceOf('Feather\\Components\\Gear\\Container', $gear = $this->feather['gear']->start('stub'));
+		$this->assertTrue($this->feather['gear']->started('stub'));
 		$this->assertInstanceOf('Feather\\Gear\\Stub\\Mock', $gear['mock']);
 	}
 
 	public function testCanDisableGear()
 	{
-		$feather = $this->stub();
-		
-		$this->assertTrue($feather['gear']->registered('stub'));
+		$this->assertTrue($this->feather['gear']->registered('stub'));
 
-		$feather['gear']->disable('stub');
+		$this->feather['gear']->disable('stub');
 
-		$this->assertTrue(!$feather['gear']->registered('stub'));
+		$this->assertTrue(!$this->feather['gear']->registered('stub'));
 	}
 
 	public function testGearEventsFire()
 	{
-		$feather = $this->stub();
-
-		$this->assertEquals('cat', $feather['gear']->first('mock.callable'));
-		$this->assertEquals('dog', $feather['gear']->first('mock.method'));
+		$this->assertEquals('cat', $this->feather['gear']->first('mock.callable'));
+		$this->assertEquals('dog', $this->feather['gear']->first('mock.method'));
 	}
-
-	public function stub()
-	{
-		$feather = Feather\Components\Support\Facade::application();
-
-		$feather['gear']->register(new Feather\Models\Gear(array(
-			'identifier' => 'stub',
-			'location'	 => 'path: ' . __DIR__ . DS . 'mock',
-			'auto'		 => false
-		)));
-
-		return $feather;
-	}
-
 }
