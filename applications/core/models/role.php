c2e10d983a3b56a5d47f2088c6a72eecf7eb97b3
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-22 21:50:44 +0930

    Initial commit of role and user models.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ applications/core/models/role.php
@@ -0,0 +1,12 @@
+<?php namespace Feather\Models;
+
+class Role extends Base {
+
+	/**
+	 * The table name.
+	 * 
+	 * @var string
+	 */
+	public static $table = 'roles';
+
+}
---
+++ applications/core/models/user.php
@@ -0,0 +1,29 @@
+<?php namespace Feather\Models;
+
+class User extends Base {
+
+	/**
+	 * The table name.
+	 * 
+	 * @var string
+	 */
+	public static $table = 'users';
+
+	/**
+	 * Timestamps are enabled.
+	 * 
+	 * @var bool
+	 */
+	public static $timestamps = true;
+
+	/**
+	 * A user can have many and belong to many roles.
+	 * 
+	 * @return object
+	 */
+	public function roles()
+	{
+		return $this->has_many_and_belongs_to('Feather\\Models\\Role', 'user_roles', 'user_id', 'role_id');
+	}
+
+}
