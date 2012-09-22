212ef3161e0d4df8eafa2d28b1d7ff4dd6521a48
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-22 23:41:29 +0930

    Initial commit of auth tests.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ tests/auth.test.php
@@ -0,0 +1,60 @@
+<?php
+
+use Mockery as M;
+
+class AuthTest extends PHPUnit_Framework_TestCase {
+
+	public $feather;
+
+	public function setUp()
+	{
+		$this->feather = Feather\Components\Support\Facade::application();
+
+		$this->feather['auth']->user = $this->user();
+	}
+
+	public function tearDown()
+	{
+		M::close();
+	}
+
+	public function testCanDoAction()
+	{
+		$this->assertTrue($this->feather['auth']->can('do: something'));
+	}
+
+	public function testCannotDoAction()
+	{
+		$this->assertTrue($this->feather['auth']->cannot('view: something'));
+	}
+
+	public function testCanCheckRole()
+	{
+		$this->assertTrue($this->feather['auth']->is('administrator'));
+		$this->assertTrue($this->feather['auth']->is('admin'));
+		$this->assertTrue($this->feather['auth']->not('boggyman'));
+	}
+
+	public function testCanCheckOnline()
+	{
+		$this->assertTrue($this->feather['auth']->online());
+	}
+
+	public function testCanCheckActivation()
+	{
+		$this->assertTrue($this->feather['auth']->activated());
+	}
+
+	public function user()
+	{
+		return new Feather\Models\User(array(
+			'roles' => array(
+				new Feather\Models\Role(array('do_something' => 1)),
+				new Feather\Models\Role(array('name' => 'Administrator')),
+				new Feather\Models\Role(array('view_something' => 0))
+			),
+			'activated' => 1
+		));
+	}
+
+}
