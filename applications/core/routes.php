619f27285bebd90826c77facb4158062274a59ab
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-23 20:41:14 +0930

    Added the main auth routes.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- applications/core/routes.php
+++ applications/core/routes.php
@@ -1,6 +1,13 @@
 <?php
 
-Route::get('(:bundle)', function()
-{
-	
-});
+Route::get('(:bundle)', array('as' => 'feather', 'uses' => 'feather core::index@index'));
+
+/*
+|--------------------------------------------------------------------------
+| User Authentication
+|--------------------------------------------------------------------------
+*/
+
+Route::any('(:bundle)/login', array('as' => 'login', 'uses' => 'feather core::index@login'));
+Route::any('(:bundle)/register', array('as' => 'register', 'uses' => 'feather core::login@register'));
+Route::get('(:bundle)/logout', array('as' => 'logout', 'uses' => 'feather core::login@logout'));
