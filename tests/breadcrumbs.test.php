d940befc4a78ee8224a705104bf501457b0bc6ed
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-23 18:58:07 +0930

    Renamed the breadcrumbs test.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ tests/breadcrumbs.test.php
@@ -0,0 +1,24 @@
+<?php
+
+class BreadcrumbsTest extends PHPUnit_Framework_TestCase {
+
+	public $feather;
+
+	public function setUp()
+	{
+		$this->feather = Feather\Components\Support\Facade::application();
+	}
+
+	public function testCanDropCrumb()
+	{
+		$this->feather['breadcrumbs']->drop('stub');
+
+		$this->assertNotEmpty($this->feather['breadcrumbs']->crumbs);
+	}
+
+	public function testCanGetHTML()
+	{
+		$this->assertEquals('<li><a href="http://:/index.php/">stub</a></li>', $this->feather['breadcrumbs']->item('/', 'stub'));
+	}
+
+}
