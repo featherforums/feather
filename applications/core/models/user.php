d73be40d09939e751d5c72e5bbb789ab11b4f615
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-23 20:55:34 +0930

    Added some getters and setters to the user model.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- applications/core/models/user.php
+++ applications/core/models/user.php
@@ -1,5 +1,8 @@
 <?php namespace Feather\Core;
 
+use Str;
+use Hash;
+
 class User extends Base {
 
 	/**
@@ -24,6 +27,47 @@ class User extends Base {
 	public function roles()
 	{
 		return $this->has_many_and_belongs_to('Feather\\Core\\Role', 'user_roles', 'user_id', 'role_id');
+	}
+
+	/**
+	 * When setting a password it must be hashed before stored in the database.
+	 * 
+	 * @param  string  $password
+	 * @return void
+	 */
+	public function set_password($password)
+	{
+		$this->set_attribute('password', Hash::make($password));
+	}
+
+	/**
+	 * Getter for a users slug.
+	 * 
+	 * @return string
+	 */
+	public function get_slug()
+	{
+		return Str::slug($this->get_attribute('username'));
+	}
+
+	/**
+	 * Getter for a users name.
+	 * 
+	 * @return string
+	 */
+	public function get_name()
+	{
+		return $this->get_attribute('username');
+	}
+
+	/**
+	 * Getter for a users avatar URL, provided by Gravatar.
+	 * 
+	 * @return string
+	 */
+	public function get_avatar()
+	{
+		return 'http://www.gravatar.com/avatar/' . md5(strtolower(trim($this->get_attribute('email'))));
 	}
 
 }
