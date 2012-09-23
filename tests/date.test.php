acf397fac0314aef1bcedd2c0047c050dacf793c
Jason Lewis <jason.lewis1991@gmail.com>
2012-09-23 12:48:34 +0930

    Refactoring the date tests.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- tests/date.test.php
+++ tests/date.test.php
@@ -9,27 +9,27 @@ class DateTest extends PHPUnit_Framework_TestCase {
 		$this->feather = Feather\Components\Support\Facade::application();
 	}
 
-	public function testSetDateAsTimestamp()
+	public function testDateSetAsTimestamp()
 	{
 		$this->assertEquals('31/01/1991', $this->feather['date']->set(strtotime('31 January 1991'))->show('d/m/Y'));
 	}
 
-	public function testSetDateAsString()
+	public function testDateSetAsString()
 	{
 		$this->assertEquals('31/01/1991', $this->feather['date']->set('31 January 1991')->show('d/m/Y'));
 	}
 
-	public function testCanSetAlternateString()
+	public function testAlternateTextOnError()
 	{
 		$this->assertEquals('cat', $this->feather['date']->set('dog')->alternate('cat')->show('d/m/Y'));
 	}
 
-	public function testCanSetPrefixString()
+	public function testCanPrefixDate()
 	{
 		$this->assertEquals('cat31/01/1991', $this->feather['date']->set('31 January 1991')->prefix('cat')->show('d/m/Y'));
 	}
 
-	public function testCanSetSuffixString()
+	public function testCanSuffixDate()
 	{
 		$this->assertEquals('31/01/1991cat', $this->feather['date']->set('31 January 1991')->suffix('cat')->show('d/m/Y'));
 	}
